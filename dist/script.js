7d33eafeeae5a17890d71bb45f0b52d1edf30c07
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-08 16:48:21 -0300

    add layout components

--- dist/script.js
+++ dist/script.js
@@ -2,10 +2,47 @@ class App extends React.Component {
   render() {
     return (
       React.createElement("div", null,
-      React.createElement("h1", null, "Pomodoro Clock")));
+      React.createElement("h1", null, "Pomodoro Clock (FCC)"),
+      React.createElement(Clock, null)));
 
 
   }}
+
+
+class Clock extends React.Component {
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement("div", { id: "break-session" },
+      React.createElement("div", { id: "break-info" },
+      React.createElement("div", { id: "break-label" }, "Break Length"),
+      React.createElement("div", { id: "break-increment" }, "+"),
+      React.createElement("div", { id: "break-length" }, "5"),
+      React.createElement("div", { id: "break-decrement" }, "-")),
+
+      React.createElement("div", { id: "session-info" },
+      React.createElement("div", { id: "session-label" }, "Session Length"),
+      React.createElement("div", { id: "session-increment" }, "+"),
+      React.createElement("div", { id: "session-length" }, "25"),
+      React.createElement("div", { id: "session-decrement" }, "-"))),
+
+
+      React.createElement(Display, null)));
+
+
+  }}
+
+
+class Display extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { id: "display" },
+      React.createElement("div", { id: "timer-label" }, "Session"),
+      React.createElement("h1", null, "25:00")));
+
+
+  }}
+
 
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
