03a2ac1ab337ec9eea8b8763fbff5eee9fc886bf
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-11 14:43:59 -0300

    add change components

--- dist/script.js
+++ dist/script.js
@@ -10,6 +10,43 @@ class App extends React.Component {
 
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeLeft: "25:00" };
+
+  }
+  changeTime(time) {
+    this.setState({
+      timeLeft: time });
+
+  }
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement(TimeSetting, null),
+      React.createElement(Display, { timeLeft: this.state.timeLeft }),
+      React.createElement(TimeFunctionality, null)));
+
+
+  }}
+
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      React.createElement("div", { id: "display" },
+      React.createElement("div", { id: "timer-label" }, "Session"),
+      React.createElement("h1", { id: "time-left" }, this.props.timeLeft)));
+
+
+  }}
+
+class TimeSetting extends React.Component {
   render() {
     return (
       React.createElement("div", null,
@@ -24,30 +61,22 @@ class Clock extends React.Component {
       React.createElement("div", { id: "session-label" }, "Session Length"),
       React.createElement("div", { id: "session-increment" }, "+"),
       React.createElement("div", { id: "session-length" }, "25"),
-      React.createElement("div", { id: "session-decrement" }, "-"))),
-
-
-      React.createElement(Display, null),
-      React.createElement("div", { id: "functionality" },
-
-      React.createElement("button", { id: "start_stop" }, "Start"),
-      React.createElement("button", { id: "reset" }, "Reset"))));
+      React.createElement("div", { id: "session-decrement" }, "-")))));
 
 
 
   }}
 
-
-class Display extends React.Component {
+class TimeFunctionality extends React.Component {
   render() {
-    return (
-      React.createElement("div", { id: "display" },
-      React.createElement("div", { id: "timer-label" }, "Session"),
-      React.createElement("h1", { id: "time-left" }, "25:00")));
+    return React.createElement("div", null,
+    React.createElement("div", { id: "functionality" },
+
+    React.createElement("button", { id: "start_stop" }, "Start"),
+    React.createElement("button", { id: "reset" }, "Reset")));
 
 
   }}
-
 
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
